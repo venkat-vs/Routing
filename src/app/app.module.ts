a843bbbfddb15da5bf2cd590e1b4e588d0815ab6
venkat <venkat.ui18@gmail.com>
2019-03-17 10:20:03 -0500

    my first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AppComponent }   from './app.component';
+
+@NgModule({
+  imports:      [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  declarations: [ AppComponent, routingComponents ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
